5f80d6624462eaecaf2e832f8693b4e8d04b02bb
Mahoudjro <165911456+Mahoudjro@users.noreply.github.com>
2024-04-29 13:56:11 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Freelance</title>
+
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+		<span class="z">Free<em>lance</em></span>
+		<ul class="header">
+			
+			<li class="x"> <img class="img" src="img/.png"> Nous contacter</li>
+			<li class="y"> <img class="img"  src="img/filter.png"> Menu</li>
+			
+        </ul>
+
+        
+
+
+
+	</header> 
+	<main>
+
+
+	  <div class="menu">
+		  <ul>
+		    <li> <img class="img1" src="img/home.png"> <a href="#"> ACCEUIL</a> </li>
+		    <li> <img class="img2" src="img/u.png"><a href="page/choixconnexion.php"> FREELANCE</a>     </li>
+		    <li> <img class="img1" src="img/a.png"><a href="page/choixconnexion.php"> PARTENAIRE</a> </li>
+		    <li><a href="#"> RESSOURCES</a> </li>
+
+	      </ul>
+
+	      <a href="page/Choxinscription.php"> <button class="button1"> Inscription</button></a> <br> <br>
+	      <a href="page/choixconnexion.php"> <button class="button1"> Connexion</button></a>
+		
+
+	  </div>
+
+	  <div class="a">
+		  <p> <span class="b"> Talent as a service:</span> <br> nous connectons les grandes entreprises avec les meilleurs talents </p> 
+		  <a href="page/Modèle.php"><button class="button"> NOTRE MODELE</button></a> <br> <br>
+		  <a href="page/contact.php"><button class="button"> NOUS CONTACTER</button></a>
+
+	  </div>
+	</main> 
+
+
+	
+
+	
+<script type="text/javascript" src="js/script.js"></script>
+</body>
+
+</html>
+
